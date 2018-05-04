5bf39cc3964680f04702b1edfb89b745da060245
RachelAya <rachel.ayateke@gmail.com>
2018-05-04 14:07:01 +0300

    loops and operation

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,22 @@
-var input=parseInt(prompt("Enter a number:"));
-alert(input);
+var nextNumber=[];
+var i;
+var aNumber=0;
+var bNumber=1;
+var input=prompt("Enter a number:");
+
+for (i=1; i<=input; i++)
+{
+  if (i<=1)
+  {
+    nextNumber+=i;
+
+  }
+  else
+  {
+    nextNumber= (aNumber+bNumber);
+    aNumber=bNumber;
+    bNumber=nextNumber;
+  }
+
+    alert (nextNumber);
+}
